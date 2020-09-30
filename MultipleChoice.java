54545d163f0925f0f60bdc76459fe3b670f13e55
Saiph0 <72117903+Saiph0@users.noreply.github.com>
2020-09-29 23:34:57 -0700

    Create MultipleChoice.java

---
+++ MultipleChoice.java
@@ -0,0 +1,28 @@
+// Name: Cynthia Luong
+// Date: 9/29/2020
+// Class: CS 3560.01
+//
+// Multiple Choice Question Class - Holds Question and Answer
+
+public class MultipleChoice implements QuestionInterface {
+
+    String q;
+    String a;
+
+    public String question(){
+        return q;
+    }
+    public String answer(){
+        return a;
+    }
+
+    public void setQuestion(String q){
+        this.q = q;
+    }
+    public void setAnswer(String a){
+        this.a = a;
+    }
+    public boolean isMultipleChoice(){
+        return true;
+    }
+}
